9cefa6663be05f9039edc56cf1c6745df475b62d
contwong <contwong@gmail.com>
2016-08-27 16:43:11 +0800

    添加配置文件和数据模型

---
+++ config/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var config = {
+  db: {
+    username: 'root',
+    password: '',
+    database: 'weibo',
+    options: {
+      host: '10.32.4.190',
+      port: '3306',
+      logging: true
+    }
+  }
+};
+
+module.exports = config;
