ad074bac4c3bd831442a254036a233767a0b0628
mlt940109 <591744873@qq.com>
2016-09-02 21:28:41 +0800

    1.增加router的get与post方法的增强方法getEx与postEx，使用该方法，可以自动包装router.get(path,func)中返回promise的func的返回值为{code:'',msg:{}}形式

--- routes/api.js
+++ routes/api.js
@@ -4,11 +4,36 @@ var express = require('express');
 var weibo   = require('../controllers/weibo');
 var router  = express.Router();
 
+
+function proxy(func){
+  return function(req,res,next){
+    func(req,res,next)
+      .then(function (data) {
+        res.json({code:0,msg: data});
+      })
+      .catch(function (error) {
+        res.json({error: error});
+      });
+  }
+}
+
+//router get扩展
+router.getEx=function(path,func) {
+  router.get(path,proxy(func))
+}
+
+//router post扩展
+router.postEx=function(path,func) {
+  router.post(path,proxy(func))
+}
+
+
 // test
 router.get('/', function(req, res, next) {
   res.json({api: 'ok'});
 });
+
 // 微博
-router.get('/weibo', weibo.getWeibo);
+router.getEx('/weibo', weibo.getWeibo);
 
 module.exports = router;
